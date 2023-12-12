2a81118eb61aa38501dcd8934de4e36a924511be
Maria Orfanaki <orfanaki2003maria@gmail.com>
2023-12-12 03:57:19 +0200

    Add files via upload

---
+++ Training 23-24/Blink_LED/blink_LED_12.ino
@@ -0,0 +1,14 @@
+const int LED = 12;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
